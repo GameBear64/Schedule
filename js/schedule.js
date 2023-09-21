2907909dc6a42e98e4a9ceac25e8fbb9382747f9
GamBar <vlpetrov02@gmail.com>
2023-09-21 11:48:16 +0300

    code refactor

--- js/schedule.js
+++ js/schedule.js
@@ -1,51 +1,58 @@
-//this can also be an external json if you are running node.js
-// let schedule2 = {
-//     Понеделник : ["Математика", "Математика", "Бел", "Бел", "Физическо", "Англиски", "Англиски"],
-//     Вторник : ["География", "Немски", "Немски", "Бел", "ИТ", "ИТ", "Свят и личност", "Свят и личност"],
-//     Сряда : ["ИТ", "ИТ", "ИТ", "Англиски", "Англиски", "Руски", "Руски"],
-//     Четвъртък : ["Бел", "Бел", "Физическо", "Физика", "Математика", "Математика", "Англиски", "ЧК"],
-//     Пeтък : ["Англиски", "Англиски", "История", "История", "Руски", "Руски", "Математика", "Ф-М-В"]
-// }
-
-let schedule = {
-    Monday: ["Mathematics", "Mathematics", "Bulgarian", "Bulgarian", "Physics", "English", "English"],
-    Tuesday: ["Geography", "German", "German", "Bulgarian", "IT", "IT", "World and Personality", "World and Personality"],
-    Wednesday: ["IT", "IT", "IT", "English", "English", "Russian", "Russian"],
-    Thursday: ["Bulgarian", "Bulgarian", "Physical", "Physics", "Mathematics", "Mathematics", "English", "Classroom Hour"],
-    Friday: ["English", "English", "History", "History", "Russian", "Russian", "Mathematics", "Module"]
-}
+import data from './data.json' assert { type: 'json' };
+let { schedule, periods } = data;
+let { start, hour, pause, lastPause, lunch } = periods;
 
 let table = document.createElement('table');
+
+function time_convert(num) {
+  var hours = Math.floor(num / 60);
+  var minutes = num % 60;
+  return `${hours}:${minutes == 0 ? '00' : minutes}`;
+}
 
 //===================doing the head=================================
 let thead = document.createElement('thead');
 let tr = document.createElement('tr');
 
 for (let i = -1; i < Object.keys(schedule).length; i++) {
-    let th = document.createElement('th');
-    i == -1 ? th.appendChild(document.createTextNode(' ')) : th.appendChild(document.createTextNode(Object.keys(schedule)[i]))
-    tr.appendChild(th)
+  let th = document.createElement('th');
+  th.innerText = i == -1 ? ' ' : Object.keys(schedule)[i];
+  tr.appendChild(th);
 }
-thead.appendChild(tr)
-table.appendChild(thead)
+thead.appendChild(tr);
+table.appendChild(thead);
 
 //===================doing the body==================================
 let tbody = document.createElement('tbody');
-let largestArrayLength = Object.values(schedule).reduce((maxI,el) => el.length > maxI ? el.length : maxI, 0)
+let largestArrayLength = Object.values(schedule).reduce((maxI, el) => (el.length > maxI ? el.length : maxI), 0);
 
 for (let n = 0; n < largestArrayLength; n++) {
-    let tr = document.createElement('tr');
+  let tr = document.createElement('tr');
 
-    for (let i = -1; i < Object.keys(schedule).length; i++) {
-        let td = i == -1 ? document.createElement('th') : document.createElement('td');
-        i == -1 ? td.appendChild(document.createTextNode(n + 1)) : td.appendChild(document.createTextNode(Object.values(schedule)[i][n] ? Object.values(schedule)[i][n] : "-"))
-        tr.appendChild(td)
+  for (let i = -1; i < Object.keys(schedule).length; i++) {
+    let td = document.createElement('td');
+
+    if (i == -1) {
+      td.innerText = `${time_convert(start)} - ${time_convert((start += hour))}`;
+
+      if (n == 2) {
+        start += lunch;
+      } else if (n == largestArrayLength - 2) {
+        start += lastPause;
+      } else {
+        start += pause;
+      }
+    } else {
+      td.innerHTML = Object.values(schedule)[i][n]?.replace(';', ' <br> ') || '-';
     }
 
-    tbody.appendChild(tr)
+    tr.appendChild(td);
+  }
+
+  tbody.appendChild(tr);
 }
 
 //================== end ==================
-table.appendChild(tbody)
+table.appendChild(tbody);
 
-document.getElementById("schedule").appendChild(table)
+document.getElementById('schedule').appendChild(table);
